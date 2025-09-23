108736cfc2e03b51b389e4d67472e4f319c551fe
unknown <muhammadshahzaib12@gmail.comgit config --global user.name MuhammadShahzaib>
2025-09-23 12:48:28 +0500

    Backend updated

--- Backend/server.js
+++ Backend/server.js
@@ -10,17 +10,16 @@ const PORT =process.env.PORT || 3000;
 connectionDb();
 app.use(express.json());
 
+
 app.use(cors());
-// app.use('/api/course',require('./routes/course'));
+
 app.get('/',(req,res)=>{
     res.send("API is running....");
 });
 app.use('/user',require('./routes/userroutes'));
-if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
-}
 
-module.exports = app;
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+});
 
  
